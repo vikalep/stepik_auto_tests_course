aa0e63c0ff1b40f78cdf356c7128dbafa7923783
vlepeshko <vlepeshko@vlepeshkos-MacBook-Pro.local>
2022-11-08 21:18:59 +0300

    add test from lesson 9

--- less9.py
+++ less9.py
@@ -7,30 +7,23 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input_first_name = browser.find_element(By.CSS_SELECTOR, ".form-control.first")
-    input_first_name.send_keys("Test")
-    input_last_name = browser.find_element(By.CSS_SELECTOR, ".form-control.second")
-    input_last_name.send_keys("Test")
-    input_email = browser.find_element((By.CSS_SELECTOR, ".form-control.second")
-    input_email.send_keys("test@test.com")
+    first_name = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+    first_name.send_keys("Test")
+    last_name = browser.find_element(By.CSS_SELECTOR, ".first_block .second") 
+    last_name.send_keys("Test")
+    email = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
+    email.send_keys("test@test.com")
 
-    # Отправляем заполненную форму
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
     time.sleep(1)
 
     welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
     welcome_text = welcome_text_elt.text
 
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
     assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
-    # закрываем браузер после всех манипуляций
     browser.quit()
